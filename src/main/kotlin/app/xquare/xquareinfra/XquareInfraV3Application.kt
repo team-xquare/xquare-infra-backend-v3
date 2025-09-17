896117dcfdbe4ce22cdaf04ca9d47817b3bbeb30
kimsaram32 <kimsaram32@proton.me>
2025-09-18 08:54:16 +0900

    chore: initial commit

---
+++ src/main/kotlin/app/xquare/xquareinfra/XquareInfraV3Application.kt
@@ -0,0 +1,11 @@
+package app.xquare.xquareinfra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XquareInfraV3Application
+
+fun main(args: Array<String>) {
+    runApplication<XquareInfraV3Application>(*args)
+}
---
+++ src/test/kotlin/app/xquare/xquareinfra/XquareInfraV3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.xquare.xquareinfra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XquareInfraV3ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
