53ce2c44aa1c14b96682420607debe6c399fc794
kimsaram32 <kimsaram32@proton.me>
2025-09-18 15:49:23 +0900

    fix: settings.gradle.kts에서의 buildSrc 참조 제거

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -1,5 +1,4 @@
 object Project {
-    const val ROOT_NAME = "xquare-infra"
     const val GROUP = "app.xquare"
     const val VERSION = "0.0.1-SNAPSHOT"
     const val DESCRIPTION = "xquare-infra-v3"
