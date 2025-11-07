6611a3a28d4a23eeefbf855d596d120a808ebdbb
kimsaram32 <kimsaram32@proton.me>
2025-11-07 17:35:44 +0900

    feat: notice 도메인 모델링

---
+++ src/main/kotlin/app/xquare/xquareinfra/domain/notice/Notice.kt
@@ -0,0 +1,13 @@
+package app.xquare.xquareinfra.domain.notice
+
+import app.xquare.xquareinfra.domain.user.User
+import java.time.LocalDateTime
+
+data class Notice(
+    val id: Long? = null,
+    val title: String,
+    val content: String,
+    val author: User,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+)
