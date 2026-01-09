4d09cd5d66ebb0b6778ef2da0d1e50cb846058aa
kimsaram32 <kimsaram32@proton.me>
2026-01-09 16:59:58 +0900

    feat: add name validations to GitOps-related requests

--- src/main/kotlin/app/xquare/xquareinfra/adapters/inbound/web/application/dtos/request/CreateApplicationRequestDto.kt
+++ src/main/kotlin/app/xquare/xquareinfra/adapters/inbound/web/application/dtos/request/CreateApplicationRequestDto.kt
@@ -1,9 +1,13 @@
 package app.xquare.xquareinfra.adapters.inbound.web.application.dtos.request
 
 import app.xquare.xquareinfra.adapters.inbound.web.application.dtos.common.configuration.ApplicationConfigurationDto
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 
 data class CreateApplicationRequestDto(
     val teamId: Long,
+    @field:Size(min = 3, max = 45)
+    @field:Pattern(regexp = "^[a-z-]+$")
     val name: String,
     val configuration: ApplicationConfigurationDto,
 )
