7449a84a6e9abbbf0518d5f9f584ebc8dea35123
박성준 <seongjun154@naver.com>
2026-03-31 20:26:11 +0900

    feat: change email message

--- src/main/kotlin/app/xquare/xquareinfra/adapters/outbound/email/SmtpEmailAdapter.kt
+++ src/main/kotlin/app/xquare/xquareinfra/adapters/outbound/email/SmtpEmailAdapter.kt
@@ -1,25 +1,21 @@
 package app.xquare.xquareinfra.adapters.outbound.email
 
 import app.xquare.xquareinfra.application.auth.ports.outbound.EmailSendPort
-import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
 
 @Component
 class SmtpEmailAdapter(
     private val mailSender: JavaMailSender,
 ) : EmailSendPort {
-    override fun send(
-        to: String,
-        subject: String,
-        body: String,
-    ) {
-        val message =
-            SimpleMailMessage().apply {
-                setTo(to)
-                setSubject(subject)
-                setText(body)
-            }
+    override fun send(to: String, subject: String, body: String) {
+        val message = mailSender.createMimeMessage()
+        MimeMessageHelper(message, true, "UTF-8").apply {
+            setTo(to)
+            setSubject(subject)
+            setText(body, true) // true = HTML
+        }
         mailSender.send(message)
     }
 }
