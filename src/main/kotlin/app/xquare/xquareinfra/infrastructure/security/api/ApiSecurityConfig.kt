162b1d8789e5f4d98dc1b0d11d7780ed4d4e773c
treegus <cowtree28@gmail.com>
2026-05-28 18:47:27 +0900

    fix: add RequestAttributeSecurityContextRepository bean

--- src/main/kotlin/app/xquare/xquareinfra/infrastructure/security/api/ApiSecurityConfig.kt
+++ src/main/kotlin/app/xquare/xquareinfra/infrastructure/security/api/ApiSecurityConfig.kt
@@ -7,6 +7,8 @@ import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository
+import org.springframework.security.web.context.SecurityContextRepository
 import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
@@ -33,4 +35,8 @@ class ApiSecurityConfig(
             .formLogin { it.disable() }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
+
+    @Bean
+    fun securityContextRepository(): SecurityContextRepository =
+        RequestAttributeSecurityContextRepository()
 }
